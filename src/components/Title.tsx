881989356102ac9ce51d03f89e09fcca6db0bb18
nauticuff <felipecarlos200x@gmail.com>
2023-07-20 16:55:01 -0700

    tailwind breakpoint fix attempt #2

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,7 @@ export default function Title() {
     
   return (
     <section className='mt-3'>
-        <h1 className="[font-family:--font-syne] uppercase text-center leading-[1] tracking-tight text-[65px] font-semibold xs:text-[16vw] xs:font-medium xl:text-[220px]">Several</h1>
+        <h1 className="[font-family:--font-syne] uppercase text-center leading-[1] tracking-tight text-[65px] font-semibold xs:text-[16vw] xs:font-medium xl:text-[220px]">Synapsis</h1>
         {/* <div className='w-full h-[2px] bg-black'></div> */}
         <Divider />
         <p className='uppercase text-[13px] text-center my-7 text-xs font-bold'>Carlos Felipe &#169; 2023 Blog Concept Showing You The Possibilities of Thinking and Expression - </p>
